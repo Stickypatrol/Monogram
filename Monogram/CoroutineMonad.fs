d8bb9ec29762a94c8c7d3982b57b0dcf4eef7c82
Bob van Schendel <bobyantius@hotmail.com>
2016-04-18 14:10:44 +0200

    first commit

---
+++ Monogram/CoroutineMonad.fs
@@ -0,0 +1,48 @@
+﻿module CoroutineMonad
+
+type Coroutine<'a,'s> = 's -> CoroutineStep<'a,'s>
+and CoroutineStep<'a,'s> =
+  | Done of 'a*'s
+  | Yield of Coroutine<'a,'s>*'s
+
+let ret x = fun s -> Done(x,s)
+
+let rec bind p k =
+  fun s ->
+    match p s with
+    | Done(a, s') -> k a s'
+    | Yield(p', s') -> Yield((bind p' k), s')
+
+type CoroutineBuilder() =
+  member this.Return(x) = ret x
+  member this.ReturnFrom(c) = c
+  member this.Bind(p,k) = bind p k
+  member this.Zero() = ret ()
+let cor = CoroutineBuilder()
+
+let yield_ = fun s -> Yield((fun s -> Done((), s)),s)
+
+let print_ x = fun s -> printfn "%A" x
+                        Done((), s)
+
+let wait_ interval =
+  let getTime = fun s -> Done(System.DateTime.Now, s)
+  cor{
+    let! t0 = getTime
+    let rec wait_() =
+      cor{
+        let! tc = getTime
+        let dt = (tc-t0).TotalSeconds
+        if dt > interval then
+          return ()
+        else
+          do! yield_
+          return! wait_()
+      }
+    do! wait_()
+  }
+
+let rec Run c s =
+  match c s with
+  | Done(c, s') -> ()
+  | Yield(c', s') -> Run c' s'
---
+++ Monogram/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+open System.Data
+open System.Data.Linq
+open FSharp.Data.TypeProviders
+open Microsoft.FSharp.Linq
+open CoroutineMonad
+
+type DBSchema = SqlDataConnection<"Data Source=145.24.200.232\SQLEXPRESS;Initial Catalog=project;User ID=mustafa;Password=root">
+let db = DBSchema.GetDataContext()
+
+db.Thefts.
