a14b0a92469d5f002568019ebf16fa49df085ea5
Mustafa Kocakaya <kocakaya_35@hotmail.com>
2016-04-18 15:03:48 +0200

    a
    aaaa

--- Monogram/Program.fs
+++ Monogram/Program.fs
@@ -4,8 +4,22 @@ open System.Data.Linq
 open FSharp.Data.TypeProviders
 open Microsoft.FSharp.Linq
 open CoroutineMonad
+open Visualization
+//
+//type DBSchema = SqlDataConnection<"Data Source=145.24.200.232\SQLEXPRESS;Initial Catalog=project;User ID=mustafa;Password=root">
+//let db = DBSchema.GetDataContext()
+//
+//let query1 =
+//        query {
+//            for row in db.Thefts do
+//            select row
+//        }
+//query1 |> Seq.iter (fun row -> printfn "%A %A" row.Date row.Time)
+[<EntryPoint>]
+let main args =
+    do exampleprogram ()
 
-type DBSchema = SqlDataConnection<"Data Source=145.24.200.232\SQLEXPRESS;Initial Catalog=project;User ID=mustafa;Password=root">
-let db = DBSchema.GetDataContext()
+    0
 
-db.Thefts.
+
+
