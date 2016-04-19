91a1a393ee5d901018c6e56bfe78bde7f0154c8e
Jef <JFConstancia@hotmail.com>
2016-04-19 10:50:14 +0200

    commit
    visualisation

--- Monogram/Program.fs
+++ Monogram/Program.fs
@@ -1,14 +1,20 @@
-﻿open System
-open System.Data
-open System.Data.Linq
-open FSharp.Data.TypeProviders
-open Microsoft.FSharp.Linq
-open CoroutineMonad
-open Visualization
+﻿module client
 
+    open System
+    open System.Data
+    open System.Data.Linq
+    open FSharp.Data.TypeProviders
+    open Microsoft.FSharp.Linq
+    open FSharp.Charting
+    open CoroutineMonad
+    open Visualization
+    open Interface
+    open System.Windows.Forms
+    open System.Drawing
 
-[<EntryPoint>]
-let main args =
-    do exampleprogram ()
+    [<EntryPoint>]
+    let main args =
+        
+        do Application.Run(Interface.form) |> ignore
 
-    0
+        0
--- Monogram/Visualization.fs
+++ Monogram/Visualization.fs
@@ -7,16 +7,11 @@ open System.Windows.Forms
 open CoroutineMonad
 
 [<STAThread>]
-let exampleprogram (list1 : seq<'a*'a>) (list2 : seq<'a*'a>) =
+let first_Chart (list1 : seq<'a*'a>) (list2 : seq<'a*'a>) =
     let myChart = Chart.Column(list1, Name = "Thefts") |> Chart.WithYAxis(Title="Test")
     let myChart2 = Chart.Column(list2, Name = "Trommels") |> Chart.WithYAxis(Title="Test") |> Chart.WithLegend
                                                                         ( InsideArea = false, 
                                                                         Alignment = StringAlignment.Center, Docking = Docking.Top)
     let custom_Chart = Chart.Combine([myChart;myChart2])
     let myChartControl = new ChartControl(custom_Chart, Dock=DockStyle.Fill)
-    let lbl = new Label(Text="my label")
-    let form = new Form(Visible = true, TopMost = true, Width = 700, Height = 500)
-    form.Controls.Add lbl
-    form.Controls.Add(myChartControl)
-    do Application.Run(form) |> ignore
-    ()
+    myChartControl
