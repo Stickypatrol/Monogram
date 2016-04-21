030f96a6533d585fd2f1072e93e8654f4a40b440
Bob van Schendel <bobyantius@hotmail.com>
2016-04-21 09:11:31 +0200

    marked the visualization functions

--- Monogram/Visualization.fs
+++ Monogram/Visualization.fs
@@ -6,7 +6,7 @@ open System.Drawing
 open System.Windows.Forms
 open CoroutineMonad
 
-[<STAThread>]
+[<STAThread>] //amount of thefts per area List<float, string>
 let first_Chart (list1 : List<'a*'b>) (list2 : List<'a*'b>) =
     let myChart = Chart.Column(list1, Name = "Thefts") |> Chart.WithYAxis(Title="Test")
     let myChart2 = Chart.Column(list2, Name = "Trommels") |> Chart.WithYAxis(Title="Test") |> Chart.WithLegend
@@ -15,7 +15,7 @@ let first_Chart (list1 : List<'a*'b>) (list2 : List<'a*'b>) =
     let custom_Chart = Chart.Combine([myChart;myChart2])
     let myChartControl = new ChartControl(custom_Chart, Dock=DockStyle.Fill)
     myChartControl
-
+    //dates of thefts and trommels respectively List<string, string>
 let second_Chart (list1 : List<'a*'a>) (list2 : List<'a*'a>) =
     let myChart = Chart.Column(list1, Name = "Thefts") |> Chart.WithYAxis(Title="Test")
     let myChart2 = Chart.Column(list2, Name = "Trommels") |> Chart.WithYAxis(Title="Test") |> Chart.WithLegend
@@ -24,6 +24,7 @@ let second_Chart (list1 : List<'a*'a>) (list2 : List<'a*'a>) =
     let custom_Chart = Chart.Combine([myChart;myChart2])
     let myChartControl = new ChartControl(custom_Chart, Dock=DockStyle.Fill)
     myChartControl
+    //List<float, float> locations of trommels
 let third_Chart (list1 : List<'a*'a>) (list2 : List<'a*'a>) =
     let myChart = Chart.Column(list1, Name = "Thefts") |> Chart.WithYAxis(Title="Test")
     let myChart2 = Chart.Column(list2, Name = "Trommels") |> Chart.WithYAxis(Title="Test") |> Chart.WithLegend
@@ -32,7 +33,7 @@ let third_Chart (list1 : List<'a*'a>) (list2 : List<'a*'a>) =
     let custom_Chart = Chart.Combine([myChart;myChart2])
     let myChartControl = new ChartControl(custom_Chart, Dock=DockStyle.Fill)
     myChartControl
-
+    //list of thefts per unit of time List<string>
 let fourth_Chart (list1 : List<'a*'a>) (list2 : List<'a*'a>) =
     let myChart = Chart.Column(list1, Name = "Thefts") |> Chart.WithYAxis(Title="Test")
     let myChart2 = Chart.Column(list2, Name = "Trommels") |> Chart.WithYAxis(Title="Test") |> Chart.WithLegend
@@ -41,7 +42,7 @@ let fourth_Chart (list1 : List<'a*'a>) (list2 : List<'a*'a>) =
     let custom_Chart = Chart.Combine([myChart;myChart2])
     let myChartControl = new ChartControl(custom_Chart, Dock=DockStyle.Fill)
     myChartControl
-
+    //list of area and corresponding thefts List<string, int>
 let fifth_Chart (list1 : List<'a*'a>) (list2 : List<'a*'a>) =
     let myChart = Chart.Column(list1, Name = "Thefts") |> Chart.WithYAxis(Title="Test")
     let myChart2 = Chart.Column(list2, Name = "Trommels") |> Chart.WithYAxis(Title="Test") |> Chart.WithLegend
